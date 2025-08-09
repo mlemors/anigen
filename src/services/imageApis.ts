e0b5bb39054a582019721f7c7e4808519fe0bb5e
mlemors <vdrms1@gmail.com>
2025-08-09 23:46:22 +0200

    Refactor ImageApiService: Remove Purr and NekoBot sources and related fetch methods

--- src/services/imageApis.ts
+++ src/services/imageApis.ts
@@ -1,9 +1,7 @@
 export enum ImageSource {
   WAIFU_IM = 'waifuIm',
   WAIFU_PICS = 'waifuPics',
-  PURR = 'purr',
   NEKOS_MOE = 'nekosMoe',
-  NEKO_BOT = 'nekoBot',
   NEKOS_API = 'nekosApi',
   NEKOS_BEST = 'nekosBest',
   NEKOS_LIFE = 'nekosLife'
@@ -62,23 +60,6 @@ export class ImageApiService {
     }
   }
 
-  static async fetchImageFromPurr(): Promise<ImageData> {
-    try {
-      const response = await this.fetchWithUserAgent('https://purrbot.site/api/img/sfw/waifu/gif');
-      const data = await response.json();
-      
-      if (data.link) {
-        return {
-          url: data.link,
-          source: ImageSource.PURR
-        };
-      }
-      throw new Error('No link found in purr response');
-    } catch (error) {
-      throw new Error(`Failed to fetch from purr: ${error}`);
-    }
-  }
-
   static async fetchImageFromNekosMoe(): Promise<ImageData> {
     try {
       const response = await this.fetchWithUserAgent('https://nekos.moe/api/v1/random/image?nsfw=false');
@@ -93,23 +74,6 @@ export class ImageApiService {
       throw new Error('No images found in nekos.moe response');
     } catch (error) {
       throw new Error(`Failed to fetch from nekos.moe: ${error}`);
-    }
-  }
-
-  static async fetchImageFromNekoBot(): Promise<ImageData> {
-    try {
-      const response = await this.fetchWithUserAgent('https://nekobot.xyz/api/image?type=waifu');
-      const data = await response.json();
-      
-      if (data.message) {
-        return {
-          url: data.message,
-          source: ImageSource.NEKO_BOT
-        };
-      }
-      throw new Error('No message found in nekobot response');
-    } catch (error) {
-      throw new Error(`Failed to fetch from nekobot: ${error}`);
     }
   }
 
@@ -170,12 +134,8 @@ export class ImageApiService {
         return this.fetchImageFromWaifuIm();
       case ImageSource.WAIFU_PICS:
         return this.fetchImageFromWaifuPics();
-      case ImageSource.PURR:
-        return this.fetchImageFromPurr();
       case ImageSource.NEKOS_MOE:
         return this.fetchImageFromNekosMoe();
-      case ImageSource.NEKO_BOT:
-        return this.fetchImageFromNekoBot();
       case ImageSource.NEKOS_API:
         return this.fetchImageFromNekosApi();
       case ImageSource.NEKOS_BEST:
@@ -191,9 +151,7 @@ export class ImageApiService {
     const names: Record<ImageSource, string> = {
       [ImageSource.WAIFU_IM]: 'Waifu.im',
       [ImageSource.WAIFU_PICS]: 'Waifu.pics',
-      [ImageSource.PURR]: 'Purr',
       [ImageSource.NEKOS_MOE]: 'Nekos.moe',
-      [ImageSource.NEKO_BOT]: 'NekoBot',
       [ImageSource.NEKOS_API]: 'Nekos API',
       [ImageSource.NEKOS_BEST]: 'Nekos.best',
       [ImageSource.NEKOS_LIFE]: 'Nekos.life'
